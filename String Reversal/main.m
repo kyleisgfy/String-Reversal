85174d8f0cb9598b696f551958e259b581354f4c
Kyle Schneider <kyleschneider@Kyles-MacBook-Pro.local>
2015-08-24 23:00:25 -0700

    Initial Commit

---
+++ String Reversal/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  String Reversal
+//
+//  Created by Kyle Schneider on 8/24/15.
+//  Copyright (c) 2015 Kyle Schneider. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
